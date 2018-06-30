21d3b64f60d51233949d0b1a3af0420ad062364e
mariogiliarde <40725068+mariogiliarde@users.noreply.github.com>
2018-06-30 19:07:05 -0300

    traduzindo o texto

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo 'Hello Word!';
+        echo 'Olá Mundo!';
         ?>
     </body>
 </html>
